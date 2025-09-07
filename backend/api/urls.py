613e6b1b18bf00ed1895b6d01f30ea88570b24b9
Kunal_Maheshwari <kunal.webdev1@gmail.com>
2025-09-07 21:22:49 +0530

    Add Django backend setup

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import ItemViewSet
+
+router = routers.DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
